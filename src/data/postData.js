bb8ce4daa78c1c7f96b92f3d605487072d2f49b4
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-08-13 13:31:46 +0900

    set-postsReducer

---
+++ src/data/postData.js
@@ -0,0 +1,17 @@
+export const postsData = [
+  {
+    id: 0,
+    title: "title0",
+    description: "description0",
+  },
+  {
+    id: 1,
+    title: "title1",
+    description: "description1",
+  },
+  {
+    id: 2,
+    title: "title2",
+    description: "description2",
+  },
+];
