55a5ab4827ac0aa46101c2b696bc2853f2645e97
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-08-22 18:24:37 +0900

    tipsList map

--- src/screens/TipsList/TipsList.js
+++ src/screens/TipsList/TipsList.js
@@ -2,47 +2,33 @@
 import React from "react";
 import CommonButton from "../../components/common/atoms/CommonButton";
 import HeaderMenu from "../../components/common/molecules/HeaderMenu";
+import { tipsData } from "../../data/tipsData";
 import "./TipsList.css";
 
 const TipsList = () => {
+  console.log(tipsData[0].img);
   return (
     <>
       <div className="inner" style={style.inner}>
         <HeaderMenu headerTitle="Tips一覧" />
         <div className="tips-list">
-          <div className="tips">
-            <div className="tips-code"></div>
-            <p className="tips-auther">
-              <span>ID : 0001 さん</span>の投稿
-            </p>
-            <p className="tips-title">JavaScriptで文字を出力する方法</p>
-            <p className="tips-desc">
-              JavaScriptではconsole.log()という関数があります。 こちらは
-              <span>...</span>
-            </p>
-          </div>
-          <div className="tips">
-            <div className="tips-code"></div>
-            <p className="tips-auther">
-              <span>ID : 0002 さん</span>の投稿
-            </p>
-            <p className="tips-title">JavaScriptで文字を出力する方法</p>
-            <p className="tips-desc">
-              JavaScriptではconsole.log()という関数があります。 こちらは
-              <span>...</span>
-            </p>
-          </div>
-          <div className="tips">
-            <div className="tips-code"></div>
-            <p className="tips-auther active">
-              <span>あなた</span>の投稿
-            </p>
-            <p className="tips-title">JavaScriptで文字を出力する方法</p>
-            <p className="tips-desc">
-              JavaScriptではconsole.log()という関数があります。 こちらは
-              <span>...</span>
-            </p>
-          </div>
+          {tipsData.map((tips, id) => {
+            return (
+              <div className="tips">
+                <div className="tips-code" key={id}>
+                  <img src={tips.img} alt="" />
+                </div>
+                <p className="tips-auther">
+                  <span>ID : {tips.id} さん</span>の投稿
+                </p>
+                <p className="tips-title">{tips.title}</p>
+                <p className="tips-desc">
+                  {tips.description}
+                  <span>...</span>
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
       <CommonButton commonBtnText="新規投稿" />
