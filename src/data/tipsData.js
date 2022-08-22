3c2dcde3269db25b195fc1f55e01aa9f86b6bd6c
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-08-22 19:30:29 +0900

    id of tips

--- src/data/tipsData.js
+++ src/data/tipsData.js
@@ -1,21 +1,24 @@
 import sampleImg from "../images/sample.png";
 export const tipsData = [
   {
-    id: 20220001,
+    id: "20220001",
+    userId: "aaa",
     title: "この文章はダミーです。文字の大きさ、量、",
     description:
       "この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。",
     img: sampleImg,
   },
   {
-    id: 20220002,
+    id: "20220002",
+    userId: "aaa",
     title: "ダミーテキスト。ダミーテキスト。ダミーテ",
     description:
       "ダミーテキスト。ダミーテキスト。ダミーテキスト。ダミーテキスト。ダミーテキスト。ダ",
     img: sampleImg,
   },
   {
-    id: 20220003,
+    id: "20220003",
+    userId: "bbb",
     title: "親譲りの無鉄砲で小供の時から損ばかりして",
     description:
       "つれづれなるまゝに、日暮らし、硯にむかひて、心にうつりゆくよしなし事を、そこはかと",
