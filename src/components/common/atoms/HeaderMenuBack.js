948e1eaa3ad700020c082fc9e2b04018f611d762
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-08-24 19:25:49 +0900

    page back in HeaderMenu

--- src/components/common/atoms/HeaderMenuBack.js
+++ src/components/common/atoms/HeaderMenuBack.js
@@ -1,10 +1,13 @@
 import React from "react";
 import BackImg from "../../../images/back-arrow.png";
+import { useNavigate } from "react-router-dom";
 
 const HeaderMenuBack = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div className="back-menu-img">
+      <div className="back-menu-img" onClick={() => navigate(-1)}>
         <img src={BackImg} alt="back" />
       </div>
     </>
