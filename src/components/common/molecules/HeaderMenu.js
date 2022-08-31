9a9415356282763c40d055247ad93bf59fe37cff
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-09-01 01:59:40 +0900

    delete unnecessary line

--- src/components/common/molecules/HeaderMenu.js
+++ src/components/common/molecules/HeaderMenu.js
@@ -3,11 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import MenuImg from "../../../images/dots.png";
 import { menuOpen, menuClose } from "../../../features/menuSlice";
 import ModalMenu from "./ModalMenu";
-import { useLocation } from "react-router-dom";
 
 const HeaderMenu = ({ headerTitle }) => {
-  const location = useLocation();
-  console.log("location: ", location);
   const dispatch = useDispatch();
   const { menuToggle } = useSelector((state) => state.menu);
 
@@ -22,10 +19,8 @@ const HeaderMenu = ({ headerTitle }) => {
     <>
       <ModalMenu menuToggle={menuToggle} menuCloseHandler={menuCloseHandler} />
       <header className="modal-menu-header">
-        {/* TipsList.jsでは無くす予定 */}
         <HeaderMenuBack />
         <div className="header-menu-title">
-          {/* モーダルメニューではスタイルを変える予定 */}
           <p>{headerTitle}</p>
         </div>
         <div className="modal-menu-img" onClick={menuOpenHandler}>
