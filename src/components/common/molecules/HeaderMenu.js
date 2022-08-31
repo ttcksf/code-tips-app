3ec16014a7ce645a925a5b0b7e53897d1b4f54bf
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-09-01 01:55:36 +0900

    revision HeaderMenuBack.js

--- src/components/common/molecules/HeaderMenu.js
+++ src/components/common/molecules/HeaderMenu.js
@@ -3,8 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import MenuImg from "../../../images/dots.png";
 import { menuOpen, menuClose } from "../../../features/menuSlice";
 import ModalMenu from "./ModalMenu";
+import { useLocation } from "react-router-dom";
 
 const HeaderMenu = ({ headerTitle }) => {
+  const location = useLocation();
+  console.log("location: ", location);
   const dispatch = useDispatch();
   const { menuToggle } = useSelector((state) => state.menu);
 
