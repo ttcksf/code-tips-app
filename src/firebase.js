82470ab42ae6dea967f20e511610f9c6d4b80234
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-09-23 16:01:39 +0900

    createAccount

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,5 +21,6 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore();
 export const storage = getStorage();
+export const auth = getAuth();
 
 export default app;
